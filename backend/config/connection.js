047ce32f0599a430a0bf7061ccb1b529ccf4699e
Fraser Elliott <fraserelliott@hotmail.com>
2025-08-03 19:04:49 +0100

    added logging to connection.js for debugging

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -4,6 +4,8 @@ if (process.env.NODE_ENV !== "production") {
 
 const Sequelize = require("sequelize");
 
+console.log(process.env);
+
 const sequelize = process.env.DATABASE_URL
     ? new Sequelize(process.env.DATABASE_URL)
     : new Sequelize(
