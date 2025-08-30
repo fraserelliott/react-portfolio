43a77b695fb6ac3f0fdf062c369739a4d804a45f
Fraser Elliott <thephrase94@gmail.com>
2025-08-30 08:55:01 +0100

    moved login endpoint to /api/users/login. moved hashing into model hooks. added script to create user.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,40 +1,56 @@
-const { Model, DataTypes } = require("sequelize");
-
-const { sequelize } = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const { sequelize } = require('../config/connection');
 
 class User extends Model {}
 
 User.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: {
-                name: "unique_email",
-                msg: "Email must be unique"
-            }
-        },
-        pwhash: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "users"
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        name: 'unique_email',
+        msg: 'Email must be unique',
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          if (user.password && user.password.trim() !== '') {
+            user.password = await bcrypt.hash(user.password, 10);
+          } else {
+            // If frontend sent empty password, keep the old one
+            user.password = user.previous('password');
+          }
+        }
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users',
+  },
 );
 
 module.exports = User;
